eb78176434951554f17f61a26690d6ef80a4b57a
Suyel Bishnu <suyel2007@gmail.com>
2024-09-30 11:07:26 +0300

    Write the test

---
+++ __tests__/carStorageFilePromise.test.js
@@ -0,0 +1,26 @@
+// carStorageFilePromise.test.js
+
+const { search } = require("../carStorageFilePromise");
+describe('Testing with key "licence" and' + ' value "ABC-1" to resolve', () => {
+  const result = [{ model: "Bored T-model", licence: "ABC-1" }];
+  test("then", () => {
+    return search("licence", "ABC-1").then((data) =>
+      expect(data).toEqual(result)
+    );
+  });
+
+  // Second version
+  test("sync-await", async () => {
+    const data = await search("licence", "ABC-1");
+    expect(data).toEqual(result);
+  });
+  // Third version
+  test("resolves", () => {
+    return expect(search("licence", "ABC-1")).resolves.toEqual(result);
+  });
+
+  // Fourth version
+  test("resolves with async", async () => {
+    await expect(search("licence", "ABC-1")).resolves.toEqual(result);
+  });
+});
