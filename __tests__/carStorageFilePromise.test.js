986432d4adfb1f2121ac7ed7d46f5f6296f90e18
Suyel Bishnu <suyel2007@gmail.com>
2024-10-07 09:49:47 +0300

    Add new test

--- __tests__/carStorageFilePromise.test.js
+++ __tests__/carStorageFilePromise.test.js
@@ -1,6 +1,7 @@
 // carStorageFilePromise.test.js
 
 const { search } = require("../carStorageFilePromise");
+
 describe('Testing with key "licence" and' + ' value "ABC-1" to resolve', () => {
   const result = [{ model: "Bored T-model", licence: "ABC-1" }];
   test("then", () => {
@@ -14,6 +15,7 @@ describe('Testing with key "licence" and' + ' value "ABC-1" to resolve', () => {
     const data = await search("licence", "ABC-1");
     expect(data).toEqual(result);
   });
+
   // Third version
   test("resolves", () => {
     return expect(search("licence", "ABC-1")).resolves.toEqual(result);
@@ -25,11 +27,82 @@ describe('Testing with key "licence" and' + ' value "ABC-1" to resolve', () => {
   });
 });
 
-describe('Testing with all parameters missing to reject', ()=>{
-    test('catch', ()=>{
-        expect.assertions(1);
-        return search()
-        .catch(err=>expect(err)
-        .toBe('parameter missing'));
-    })
-})
+describe("Testing with all parameters missing to reject", () => {
+
+    //First version
+  test("catch", () => {
+    expect.assertions(1);
+    return search().catch((err) => expect(err).toBe("parameter missing"));
+  });
+
+  // Second version
+  test('async-await', async()=>{
+    expect.assertions(1);
+    try {
+        await search();
+    } catch (err) {
+        expect(err).toBe('parameter missing');
+
+    }
+  });
+
+  // Third version
+  test("rejects", () => {
+    return expect(search()).rejects.toBe("parameter missing");
+  });
+
+  // Fourth version
+  test("rejects with async", async () => {
+    await expect(search()).rejects.toBe("parameter missing");
+  });
+});
+
+describe('test with key "model"',()=>{
+    const resultForBoredTmodel = [
+      { "model": "Bored T-model", "licence": "ABC-1" },
+      { "model": "Bored T-model", "licence": "GTF-10" }
+    ];
+    const resultForNova = [
+      { "model": "Noval", "licence": "XYZ-123" }
+    ];
+    test('model "Bored T-model"', ()=>{
+        return expect(search('model', 'Bored T-model'))
+        .resolves.toEqual(resultForBoredTmodel)
+    });
+
+  test('model "Noval"', () => {
+    return expect(search("model", "Noval")).resolves.toEqual(resultForNova);
+  });
+
+    test('model X', ()=>{
+        return expect(search("model", "X")).resolves.toEqual([]);
+    });
+
+    test('one parameter missing', ()=>{
+        return expect(search('model'))
+        .rejects.toBe("parameter missing");
+    });
+});
+   
+
+describe('Testing with key "licence"', ()=>{
+        test('licence "X"', ()=>{
+            return expect(search('licence', 'X'))
+            .resolves.toEqual([]);
+        });
+    });
+
+    describe('Test wrong key', () => {
+      test('key "X" with value "ABC-1"', () => {
+        return expect(search("licence", "X")).resolves.toEqual([]);
+      });
+    });
+    describe("Test read error", () => {
+      const search = initSearch("x.json");
+
+      test("Missing file", () => {
+        return expect(search("x", "x")).rejects.toBe("read error");
+      });
+    });
+
+
